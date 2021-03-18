68ef2d559142811f47e58fac3ff682d2de3088f7
jaeskim <jaeskim@student.42seoul.kr>
2021-03-19 03:39:02 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-VER=v0.1
+# Color
+CL_BOLD="\e[1m"
+CL_DIM="\e[2m"
+CL_UDLINE="\e[4m"
 
-#build all docker containers
+NO_COLOR="\e[0m"
 
-#build nginx
-docker build -t ft_nginx:${VER} ./srcs/nginx
+FG_WHITE="${NO_COLOR}\e[0;37m"
+FG_TEXT="${NO_COLOR}\e[38;2;189;147;249m"
+FG_TEXT_PRIMARY="${NO_COLOR}${CL_BOLD}\e[38;2;255;121;198m"
 
-#build nginx
-docker build -t ft_nginx:${VER} ./srcs/nginx
+NL="${NO_COLOR}\n"
+
+# START!
+
+printf "☸ ${FG_TEXT}Setup ${FG_TEXT_PRIMARY}ft_services${FG_TEXT}!${NL}"
+
+# build all docker containers
+printf "🐳 ${FG_TEXT}Build all ${FG_TEXT_PRIMARY}Docker Images${FG_TEXT}!${NL}"
+# build nginx
+printf "🚏 ${FG_TEXT}Build ${FG_TEXT_PRIMARY}nginx Images${FG_TEXT}!${NL}"
+docker build -t nginx ./srcs/nginx
+
+
--- srcs/nginx/entrypoint.sh
+++ srcs/nginx/entrypoint.sh
@@ -1,6 +1,2 @@
 # start nginx
-# /usr/sbin/nginx -g "daemon off;"
-/usr/sbin/nginx
-
-# tail logs & errors
-tail -f /var/log/nginx/access.log /var/log/nginx/error.log
+/usr/sbin/nginx -g "daemon off;"
